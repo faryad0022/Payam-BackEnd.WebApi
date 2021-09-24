2ead43a913245c3c10d23807ef0a5f5844f2e1cc
Administrator <admin@example.com>
2021-09-24 06:59:44 +0300

    Add Swagger to project

--- BackEnd.WebApi/Startup.cs
+++ BackEnd.WebApi/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -43,6 +44,32 @@ namespace BackEnd.WebApi
                 .Build()
                 );
 
+            #region swagger
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
+            #endregion
 
             #region Add DbContext
             services.AddApplicationDbContext(Configuration);
@@ -92,8 +119,21 @@ namespace BackEnd.WebApi
         {
             if (env.IsDevelopment())
             {
+
+
                 app.UseDeveloperExceptionPage();
             }
+            #region swagger
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            #endregion
             app.UseStaticFiles();
 
             app.UseCors(Configuration["Cors:PolicyString"]);
