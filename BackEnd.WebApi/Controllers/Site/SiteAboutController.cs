7b8a577dc612ee50b6e3a768edfb682ce4d2eff9
Administrator <admin@example.com>
2021-12-30 14:34:27 +0330

    about page for main site is done

---
+++ BackEnd.WebApi/Controllers/Site/SiteAboutController.cs
@@ -0,0 +1,33 @@
+﻿using BackEnd.Core.Services.Interfaces;
+using BackEnd.Core.utilities.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.WebApi.Controllers.Site
+{
+
+    public class SiteAboutController : SiteBaseController
+    {
+        #region Constructor
+        private readonly IAboutService aboutService;
+        public SiteAboutController(IAboutService aboutService)
+        {
+            this.aboutService = aboutService;
+        }
+        #endregion
+
+        #region Get
+        [HttpGet("get-about")]
+        public async Task<IActionResult> GetFilterLogos()
+        {
+            var about = await aboutService.GetAboutAsync();
+
+            return JsonResponseStatus.Success(about);
+        }
+        #endregion
+    }
+}
